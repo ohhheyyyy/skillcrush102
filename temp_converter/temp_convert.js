3dbb390b8dfc4fdb56303360ce86f17294f4144f
ohhheyyyy <ashley.hartt@gmail.com>
2015-07-05 00:05:27 -0400

    Move HTML, CSS, and JS temp converter files to unique directory

---
+++ temp_converter/temp_convert.js
@@ -0,0 +1,26 @@
+// Celsius / Fahrenheit Temperature Converter
+
+var processForm = function () {
+    
+    var temperature = Number(document.tempForm.temp.value);
+    var tempType;
+    var result;
+
+    for (var i=0; i < document.tempForm.choice.length; i++) {
+        
+        if (document.tempForm.choice[i].checked) {
+            tempType = document.tempForm.choice[i].value;
+        }
+    }
+
+    if (tempType == 'fahrenheit') {
+        result = temperature * 9/5 + 32;
+    }
+    
+    else {
+        result = (temperature -  32)  *  5/9;
+    }
+    
+    // Assign the result field value
+    document.tempForm.resultField.value=result;
+};
