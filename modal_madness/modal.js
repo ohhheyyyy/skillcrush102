85c07c6824e2731818bf4ebb1c9e4c9335e669d2
ohhheyyyy <ashley.hartt@gmail.com>
2015-07-05 22:08:46 -0400

    Add HTML, CSS, and JavaScript files to show / hide a modal

---
+++ modal_madness/modal.js
@@ -0,0 +1,36 @@
+// modal.js
+
+// wrap function in jQuery document.ready so 
+// JavaScript loads after page is finished loading
+
+$(document).ready(function() {
+
+// define a variable for each button
+
+var showModalButton = document.getElementById("showModalButton");
+var hideModalButton = document.getElementById("hideModalButton");
+
+// define a variable for the modal element
+
+var modalElement = document.getElementById("modalContainer");
+
+
+// define function to show the modal
+
+function showModal() {
+	modalElement.style.display = 'block';
+	showModalButton.style.display = 'none';
+}
+
+// define function to hide the modal
+
+function hideModal() {
+	modalElement.style.display = 'none';
+	showModalButton.style.display = 'block';
+}
+
+// call functions to show or hide the modal when clicked
+
+showModalButton.onclick = showModal;
+hideModalButton.onclick = hideModal;
+})
